853adde23671d4ae761ce1a544b77c7af19455a8
mrdth <mrdth@zeventien.org>
2021-05-18 21:52:41 +0100

    Add my standard dev helper packages & config

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @mixin IdeHelperCharacter
+ */
 class Character extends Model
 {
     use HasFactory;
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @mixin IdeHelperItem
+ */
 class Item extends Model
 {
     use HasFactory;
